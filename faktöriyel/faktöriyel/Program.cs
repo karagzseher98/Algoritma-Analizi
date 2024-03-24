d4bd42c6d152e824e50e1cda05458e7c45537224
karagzseher98 <164140489+karagzseher98@users.noreply.github.com>
2024-03-24 21:22:27 +0300

    FaktoriyelBulmaİteratif

---
+++ faktöriyel/faktöriyel/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Faktöriyelini almak istediğiniz sayıyı girin: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        long sonuc = Faktoriyel(sayi);
+
+        Console.WriteLine($"Faktöriyel({sayi}) = {sonuc}");
+
+        Console.ReadLine();
+    }
+
+    static long Faktoriyel(int n)
+    {
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Faktoriyel(n - 1);
+        }
+    }
+}
