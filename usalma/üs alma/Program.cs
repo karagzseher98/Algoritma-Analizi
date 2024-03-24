9cca095fe0d907ca47f1556f3087c71a57f24ecd
karagzseher98 <164140489+karagzseher98@users.noreply.github.com>
2024-03-24 22:00:33 +0300

    üs_alma

---
+++ usalma/üs alma/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Tabanı ve üssü girin (örneğin, 2^3 için 2 3): ");
+        string[] input = Console.ReadLine().Split(' ');
+
+        if (input.Length == 2 && double.TryParse(input[0], out double taban) && int.TryParse(input[1], out int us))
+        {
+            Console.WriteLine($"{taban}^{us} = {UsAl(taban, us)}");
+        }
+        else
+        {
+            Console.WriteLine("Hatalı giriş!");
+        }
+
+        Console.ReadLine();
+    }
+
+    static double UsAl(double taban, int us) => (us == 0) ? 1 : (us > 0) ? taban * UsAl(taban, us - 1) : 1 / (taban * UsAl(taban, -us - 1));
+}
