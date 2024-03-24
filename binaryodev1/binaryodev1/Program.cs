ef88692f51a97cda88913102fb96f92a575d8232
karagzseher98 <164140489+karagzseher98@users.noreply.github.com>
2024-03-24 21:43:33 +0300

    binary_iteratif

---
+++ binaryodev1/binaryodev1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    // Onluk sayıyı ikilik sayıya çeviren fonksiyon
+    static string DecimalToBinary(int decimalNumber)
+    {
+        if (decimalNumber == 0)
+        {
+            return "0";
+        }
+
+        string binary = "";
+        while (decimalNumber > 0)
+        {
+            int remainder = decimalNumber % 2;
+            binary = remainder + binary;
+            decimalNumber = decimalNumber / 2;
+        }
+
+        return binary;
+    }
+
+    static void Main(string[] args)
+    {
+        // Onluk sayıyı kullanıcıdan al
+        Console.Write("Lütfen bir onluk sayı girin: ");
+        int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+        // İkilik sayıya dönüştür
+        string binaryNumber = DecimalToBinary(decimalNumber);
+
+        // Sonucu ekrana yazdır
+        Console.WriteLine($"İkilik karşılığı: {binaryNumber}");
+    }
+}
