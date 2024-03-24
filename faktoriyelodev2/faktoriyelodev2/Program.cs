8736e7a4896cc0baf4f347def126635787e70f55
karagzseher98 <164140489+karagzseher98@users.noreply.github.com>
2024-03-24 21:30:03 +0300

    faktoriyel recursive

---
+++ faktoriyelodev2/faktoriyelodev2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Faktoriyel
+{
+    static int FaktoriyelHesapla(int n)
+    {
+        // Sonuç için bir değişken oluştur
+        int sonuc = 1;
+
+        // 1'den n'e kadar olan sayıları çarp
+        for (int i = 2; i <= n; i++)
+        {
+            sonuc *= i;
+        }
+
+        return sonuc;
+    }
+
+    static void Main(string[] args)
+    {
+        // Kullanıcıdan faktoriyeli hesaplanacak sayıyı al
+        Console.Write("Faktoriyeli hesaplanacak sayıyı girin: ");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+
+        // Faktoriyeli hesapla ve ekrana yazdır
+        int faktoriyel = FaktoriyelHesapla(sayi);
+        Console.WriteLine($"{sayi}! = {faktoriyel}");
+    }
+}
