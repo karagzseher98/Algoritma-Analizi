101c80a0604a878f6f628be5713f1f3bbd7eeae5
karagzseher98 <164140489+karagzseher98@users.noreply.github.com>
2024-03-24 21:36:42 +0300

    mergeshort

---
+++ mergeshort/mergeshort/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+ internal class MergeSortAlgorithm
+{
+    static void Main(string[] args)
+    {
+        int[]dizi= MergeSort(new int[]){ 15,55,64,14,48,19,32,28,3,48,9,24};
+
+
+
+
+
+    }
+}
